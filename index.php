a1a97e168a5d3c802f12e7351b13dab0a4f68db2
n-peugnet <n.peugnet@free.fr>
2019-11-07 01:42:54 +0100

    feat: better sentry conf + send catched errors

--- index.php
+++ index.php
@@ -13,13 +13,24 @@ require('./vendor/autoload.php');
 
 $app = new Wcms\Application();
 $app->wakeup();
-Sentry\init(['dsn' => Wcms\Config::sentrydsn()]);
+Sentry\init([
+    'dsn' => Wcms\Config::sentrydsn(),
+    'release' => 'w_cms_v' . getversion(),
+    'project_root' => 'app',
+]);
+Sentry\configureScope(function (Sentry\State\Scope $scope): void {
+    $scope->setUser([
+        'id' => Wcms\Config::url(),
+        'username' => Wcms\Config::basepath(),
+    ]);
+});
 
 try {
     $matchoper = new Wcms\Routes();
     $matchoper->match();
 
 } catch (Exception $e) {
+    Sentry\captureException($e);
     echo '<h1>⚠ Woops ! There is a little problem : </h1>', $e->getMessage(), "\n";
 }
 
