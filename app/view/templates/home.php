199694807496e98c47f6069eb3db6d925332968c
vincent-peugnet <v.peugnet@free.fr>
2020-03-26 17:23:37 +0100

    fix double search bar

--- app/view/templates/home.php
+++ app/view/templates/home.php
@@ -82,23 +82,6 @@
                 </div>
 
 
-                <div id="deepsearchbar" class="hidephone">
-                    <form action="<?= $this->url('home') ?>" method="get">
-                        <input type="text" name="search" value="<?= $deepsearch ?>" id="deepsearch" placeholder="deep search">
-                        <input type="checkbox" name="title" id="deeptitle" value="1" <?= $searchopt['title'] ? 'checked' : '' ?>>
-                        <label for="deeptitle">title</label>
-                        <input type="checkbox" name="description" id="deepdescription" value="1"  <?= $searchopt['description'] ? 'checked' : '' ?>>
-                        <label for="deepdescription">description</label>
-                        <input type="checkbox" name="content" id="deepcontent" value="1"  <?= $searchopt['content'] ? 'checked' : '' ?>>
-                        <label for="deepcontent" title="Markdown content : MAIN, HEADER, NAV, ASIDE, FOOTER">content</label>
-                        <input type="checkbox" name="other" id="deepother" value="1"  <?= $searchopt['other'] ? 'checked' : '' ?>>
-                        <label for="deepother" title="Structure content : BODY, CSS, Javascript">other</label>
-                        <input type="checkbox" name="case" id="deepcase" value="1"  <?= $searchopt['casesensitive'] ? 'checked' : '' ?>>
-                        <label for="deepcase" title="Case sensitive or not">case sensitive</label>
-                        <input type="submit" value="search">
-                    </form>
-                </div>
-
                 <div class="scroll">
 
                     <table id="home2table">
