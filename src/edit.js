4198be65d4962487622eeb59d50ca9a7a320c098
Nicolas Peugnet <nicolas@club1.fr>
2022-11-04 21:25:19 +0100

    syntax highlight wikilinks close #238

--- src/edit.js
+++ src/edit.js
@@ -20,6 +20,7 @@ CodeMirror.defineSimpleMode('wcms', {
             token: 'wcms',
             next: 'wcms',
         },
+        { regex: /\[\[[a-z0-9\-\_]+\]\]/, token: 'wikilink', next: 'start' },
         { regex: /<!--/, token: 'comment', next: 'comment' },
     ],
     // 'wcms' mode, for each macro, if there is parameters, pass to its associated mode
