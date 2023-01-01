708b6b4d64bf7bbce1773bd83993a8a86b890345
vincent-peugnet <vincent-peugnet@riseup.net>
2023-01-01 17:20:48 +0100

    some UI upgrades in home view

--- app/view/templates/homebookmark.php
+++ app/view/templates/homebookmark.php
@@ -4,15 +4,12 @@
     <div class="block">
 
         <h2>Bookmarks</h2>
-        <strong>public</strong>
+        <h3>public</h3>
 
         <table>
             <tbody>
-                <?php
-
-
- foreach ($publicbookmarks as $bookmark) { ?>
-                <tr>
+                <?php foreach ($publicbookmarks as $bookmark) { ?>
+                    <tr>
                         <td>
                             <a
                                 href="<?= $this->url($bookmark->route(), $bookmark->params(), $bookmark->query()) ?>"
@@ -20,10 +17,13 @@
                                 class="bookmark"
                                 title="<?= $bookmark->description() ?>"
                             >
-                            <?= $bookmark->icon() ?> <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
+                                <?= $bookmark->icon() ?>
+                                <span>
+                                    <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
+                                </span>
                             </a>
                         </td>
-                        <td>
+                        <td class="rss">
                             <?php if($bookmark->ispublished()){ ?>
                                 <a href="<?= Wcms\Servicerss::atomfile($bookmark->id()) ?>" target="_blank" title="show Atom XML file">
                                     <i class="fa fa-rss"></i>
@@ -35,7 +35,7 @@
             </tbody>
         </table>
 
-            <strong>personal</strong>
+            <h3>personal</h3>
         
         <table>
             <?php foreach ($personalbookmarks as $bookmark) { ?>
@@ -47,7 +47,10 @@
                             class="bookmark"
                             title="<?= $bookmark->description() ?>"
                         >
-                        <?= $bookmark->icon() ?> <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
+                            <?= $bookmark->icon() ?>
+                            <span>
+                                <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
+                            </span>
                         </a>
                     </td>
                 </tr>
--- app/view/templates/homeopt.php
+++ app/view/templates/homeopt.php
@@ -13,9 +13,11 @@
             </summary>
 
 
-                <form action="./" method="get">
-                    <input type="submit" name="submit" value="filter">
-                    ⬅<input type="submit" name="submit" value="reset">
+                <form action="<?= $this->url('home') ?>" method="get">
+                    <input type="submit" name="submit" value="filter" class="filter">
+                    <?php if ($opt->isfiltered()) { ?>
+                        <input type="submit" name="submit" value="reset">
+                    <?php } ?>
 
                     <div id="optfield">
 
@@ -23,21 +25,22 @@
 
                         <fieldset data-default="<?= $opt->isdefault('limit') ? '1' : '0' ?>">
                             <legend>Sort</legend>
-                            <select name="sortby" id="sortby">
-                                <?php
-                                foreach (Wcms\Model::COLUMNS as $col) {
-                                    echo '<option value="' . $col . '" ' . ($opt->sortby() == $col ? "selected" : "") . '>' . $col . '</option>';
-                                }
-                                ?>
-                            </select>
-                            </br>
-                            <input type="radio" id="asc" name="order" value="1" <?= $opt->order() == '1' ? "checked" : "" ?> /><label for="asc">ascending</label>
-                            </br>
-                            <input type="radio" id="desc" name="order" value="-1" <?= $opt->order() == '-1' ? "checked" : "" ?> /><label for="desc">descending</label>
-                            <br>
-                            <input type="number" name="limit" id="limit" value="<?= $opt->limit() ?>" min="0" max="9999">
-                            <label for="limit">limit</label>
-
+                            <div>
+                                <select name="sortby" id="sortby">
+                                    <?php
+                                    foreach (Wcms\Model::COLUMNS as $col) {
+                                        echo '<option value="' . $col . '" ' . ($opt->sortby() == $col ? "selected" : "") . '>' . $col . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                                </br>
+                                <input type="radio" id="asc" name="order" value="1" <?= $opt->order() == '1' ? "checked" : "" ?> /><label for="asc">ascending</label>
+                                </br>
+                                <input type="radio" id="desc" name="order" value="-1" <?= $opt->order() == '-1' ? "checked" : "" ?> /><label for="desc">descending</label>
+                                <br>
+                                <input type="number" name="limit" id="limit" value="<?= $opt->limit() ?>" min="0" max="9999">
+                                <label for="limit">limit</label>
+                            </div>
                         </fieldset>
 
 
@@ -45,12 +48,14 @@
 
                         <fieldset data-default="<?= $opt->isdefault('secure') ? '1' : '0' ?>">
                             <legend>Privacy</legend>
-                            <ul>
-                                <li><input type="radio" id="4" name="secure" value="4" <?= $opt->secure() == 4 ? "checked" : "" ?> /><label for="4">all</label></li>
-                                <li><input type="radio" id="2" name="secure" value="2" <?= $opt->secure() == 2 ? "checked" : "" ?> /><label for="2">not published</label></li>
-                                <li><input type="radio" id="1" name="secure" value="1" <?= $opt->secure() == 1 ? "checked" : "" ?> /><label for="1">private</label></li>
-                                <li><input type="radio" id="0" name="secure" value="0" <?= $opt->secure() == 0 ? "checked" : "" ?> /><label for="0">public</label></li>
-                            </ul>
+                            <div>
+                                <ul>
+                                    <li><input type="radio" id="4" name="secure" value="4" <?= $opt->secure() == 4 ? "checked" : "" ?> /><label for="4">all</label></li>
+                                    <li><input type="radio" id="2" name="secure" value="2" <?= $opt->secure() == 2 ? "checked" : "" ?> /><label for="2">not published</label></li>
+                                    <li><input type="radio" id="1" name="secure" value="1" <?= $opt->secure() == 1 ? "checked" : "" ?> /><label for="1">private</label></li>
+                                    <li><input type="radio" id="0" name="secure" value="0" <?= $opt->secure() == 0 ? "checked" : "" ?> /><label for="0">public</label></li>
+                                </ul>
+                            </div>
                         </fieldset>
 
 
@@ -58,40 +63,40 @@
 
                         <fieldset data-default="<?= $opt->isdefault('tagfilter') && $opt->tagcompare() != 'EMPTY' ? '1' : '0' ?>">
                             <legend>Tag</legend>
+                            <div>
+                                <input type="hidden" name="tagnot" value="0" <?= !$opt->tagnot() ? "checked" : '' ?>>
+                                <input type="checkbox" name="tagnot" id="tagnot" value="1" <?= $opt->tagnot() ? "checked" : '' ?>>
+                                <label for="tagnot">NOT</label>
+                                <br>
 
-                            <input type="hidden" name="tagnot" value="0" <?= !$opt->tagnot() ? "checked" : '' ?>>
-                            <input type="checkbox" name="tagnot" id="tagnot" value="1" <?= $opt->tagnot() ? "checked" : '' ?>>
-                            <label for="tagnot">NOT</label>
-                            <br>
+                                
+                                <input type="radio" id="tag_OR" name="tagcompare" value="OR" ' . <?= $opt->tagcompare() == "OR" ? "checked" : "" ?> >
+                                <label for="tag_OR">OR</label>
+                                <input type="radio" id="tag_AND" name="tagcompare" value="AND" <?= $opt->tagcompare() == "AND" ? "checked" : "" ?>>
+                                <label for="tag_AND">AND</label>
+                                <input type="radio" id="tag_EMPTY" name="tagcompare" value="EMPTY" <?= $opt->tagcompare() == "EMPTY" ? "checked" : "" ?>>
+                                <label for="tag_EMPTY">EMPTY</label>
+                                
+                                <hr>
 
-                            
-                            <input type="radio" id="tag_OR" name="tagcompare" value="OR" ' . <?= $opt->tagcompare() == "OR" ? "checked" : "" ?> >
-                            <label for="tag_OR">OR</label>
-                            <input type="radio" id="tag_AND" name="tagcompare" value="AND" <?= $opt->tagcompare() == "AND" ? "checked" : "" ?>>
-                            <label for="tag_AND">AND</label>
-                            <input type="radio" id="tag_EMPTY" name="tagcompare" value="EMPTY" <?= $opt->tagcompare() == "EMPTY" ? "checked" : "" ?>>
-                            <label for="tag_EMPTY">EMPTY</label>
-                            
-                            <hr>
-
-                            <ul>
-                                <?php foreach ($opt->taglist() as $tagfilter => $count) { ?>
-                                    <li>
-                                        <input
-                                            type="checkbox"
-                                            name="tagfilter[]"
-                                            id="tag_<?= $tagfilter ?>"
-                                            value="<?= $tagfilter ?>"
-                                            <?= in_array($tagfilter, $opt->tagfilter()) ? 'checked' : '' ?>
-                                        />
-                                        <label for="tag_<?= $tagfilter ?>">
-                                            <span class="list-label"><?= $tagfilter ?></span>
-                                            <span class="counter tag_<?= $tagfilter ?>"><?= $count ?></span>
-                                        </label>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-
+                                <ul>
+                                    <?php foreach ($opt->taglist() as $tagfilter => $count) { ?>
+                                        <li>
+                                            <input
+                                                type="checkbox"
+                                                name="tagfilter[]"
+                                                id="tag_<?= $tagfilter ?>"
+                                                value="<?= $tagfilter ?>"
+                                                <?= in_array($tagfilter, $opt->tagfilter()) ? 'checked' : '' ?>
+                                            />
+                                            <label for="tag_<?= $tagfilter ?>">
+                                                <span class="list-label"><?= $tagfilter ?></span>
+                                                <span class="counter tag_<?= $tagfilter ?>"><?= $count ?></span>
+                                            </label>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
                         </fieldset>
 
 
@@ -100,43 +105,46 @@
                         <fieldset data-default="<?= $opt->isdefault('authorfilter') && $opt->authorcompare() !== 'EMPTY' ? '1' : '0' ?>">
                             <legend>Author(s)</legend>
                                     
-                            
-                            <input type="radio" id="author_OR" name="authorcompare" value="OR" ' . <?= $opt->authorcompare() == "OR" ? "checked" : "" ?>>
-                            <label for="author_OR">OR</label>
-                            <input type="radio" id="author_AND" name="authorcompare" value="AND" <?= $opt->authorcompare() == "AND" ? "checked" : "" ?>>
-                            <label for="author_AND">AND</label>
-                            <input type="radio" id="author_EMPTY" name="authorcompare" value="EMPTY" <?= $opt->authorcompare() == "EMPTY" ? "checked" : "" ?>>
-                            <label for="author_EMPTY">EMPTY</label>
-                            
-                            <ul>
-                                <?php foreach ($opt->authorlist() as $authorfilter => $count) { ?>
+                            <div>
+                                <input type="radio" id="author_OR" name="authorcompare" value="OR" ' . <?= $opt->authorcompare() == "OR" ? "checked" : "" ?>>
+                                <label for="author_OR">OR</label>
+                                <input type="radio" id="author_AND" name="authorcompare" value="AND" <?= $opt->authorcompare() == "AND" ? "checked" : "" ?>>
+                                <label for="author_AND">AND</label>
+                                <input type="radio" id="author_EMPTY" name="authorcompare" value="EMPTY" <?= $opt->authorcompare() == "EMPTY" ? "checked" : "" ?>>
+                                <label for="author_EMPTY">EMPTY</label>
+                                
+                                <ul>
+                                    <?php foreach ($opt->authorlist() as $authorfilter => $count) { ?>
 
-                                    <li>
-                                        <input
-                                            type="checkbox"
-                                            name="authorfilter[]"
-                                            id="author_<?= $authorfilter ?>"
-                                            value="<?= $authorfilter ?>"
-                                            <?= in_array($authorfilter, $opt->authorfilter()) ? 'checked' : '' ?>
-                                        />
-                                        <label for="author_<?= $authorfilter ?>">
-                                            <span class="list-label"><?= $authorfilter ?></span>
-                                            <span class="counter"><?= $count ?></span>
-                                        </label>
-                                    </li>
-                                <?php } ?>
-                            </ul>
+                                        <li>
+                                            <input
+                                                type="checkbox"
+                                                name="authorfilter[]"
+                                                id="author_<?= $authorfilter ?>"
+                                                value="<?= $authorfilter ?>"
+                                                <?= in_array($authorfilter, $opt->authorfilter()) ? 'checked' : '' ?>
+                                            />
+                                            <label for="author_<?= $authorfilter ?>">
+                                                <span class="list-label"><?= $authorfilter ?></span>
+                                                <span class="counter"><?= $count ?></span>
+                                            </label>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            </div>
 
                         </fieldset>
 
                         <fieldset data-default="<?= $opt->isdefault('since') && $opt->isdefault('until') ? '1' : '0' ?>">
                             <legend>Date & time</legend>
-                            <label for="since">since</label>
-                            </br>
-                            <input type="datetime-local" name="since" id="since" value="<?= !is_null($opt->since()) ? $opt->since('Y-m-d\TH:i') : "" ?>">
-                            <label for="until">until</label>
-                            </br>
-                            <input type="datetime-local" name="until" id="until" value="<?= !is_null($opt->until()) ? $opt->until('Y-m-d\TH:i') : "" ?>">
+                            <div>
+                                <label for="since">since</label>
+                                </br>
+                                <input type="datetime-local" name="since" id="since" value="<?= !is_null($opt->since()) ? $opt->since('Y-m-d\TH:i') : "" ?>">
+                                <label for="until">until</label>
+                                </br>
+                                <input type="datetime-local" name="until" id="until" value="<?= !is_null($opt->until()) ? $opt->until('Y-m-d\TH:i') : "" ?>">
+                            </div>
                         </fieldset>
 
 
@@ -144,22 +152,24 @@
 
                         <fieldset data-default="<?= $opt->isdefault('linkto') ? '1' : '0' ?>">
                             <legend>Link to</legend>
-
-                        <label for="linkto" title="filter pages that have links pointing to the following selected page">Link pointing to</label>
-                        </br>
-                            <select name="linkto" id="linkto">
-                                <option value="" selected>-- not set --</option>
-                                <?php
-                                foreach ($opt->pageidlist() as $id) {
-                                    if ($id === $opt->linkto()) {
-                                        $selected = ' selected';
-                                    } else {
-                                        $selected = '';
+                            
+                            <div>
+                                <label for="linkto" title="filter pages that have links pointing to the following selected page">Link pointing to</label>
+                                </br>
+                                <select name="linkto" id="linkto">
+                                    <option value="" selected>-- not set --</option>
+                                    <?php
+                                    foreach ($opt->pageidlist() as $id) {
+                                        if ($id === $opt->linkto()) {
+                                            $selected = ' selected';
+                                        } else {
+                                            $selected = '';
+                                        }
+                                        echo '<option value="' . $id . '"' . $selected . '>' . $id . '</option>';
                                     }
-                                    echo '<option value="' . $id . '"' . $selected . '>' . $id . '</option>';
-                                }
-                                ?>
-                            </select>
+                                    ?>
+                                </select>
+                            </div>
                         </fieldset>
 
 
@@ -169,12 +179,12 @@
                             <legend>Other</legend>
 
                             
+                            <div>
+                                <input type="hidden" name="invert" value="0" <?= !$opt->invert() ? 'checked' : '' ?>>
+                                <input type="checkbox" name="invert" value="1" id="invert" <?= $opt->invert() ? 'checked' : '' ?>>
 
-                        <input type="hidden" name="invert" value="0" <?= !$opt->invert() ? 'checked' : '' ?>>
-                        <input type="checkbox" name="invert" value="1" id="invert" <?= $opt->invert() ? 'checked' : '' ?>>
-
-                        <label for="invert">invert</></br>
-
+                                <label for="invert">invert</label></br>
+                                </div>
 
                             
                         </fieldset>
@@ -184,8 +194,10 @@
                     <input type="hidden" name="display" value="<?= $display ?>">
 
 
-                    <input type="submit" name="submit" value="filter">
-                    ⬅<input type="submit" name="submit" value="reset">
+                    <input type="submit" name="submit" value="filter" class="filter">
+                    <?php if ($opt->isfiltered()) { ?>
+                        <input type="submit" name="submit" value="reset">
+                    <?php } ?>
 
                 </form>
 
