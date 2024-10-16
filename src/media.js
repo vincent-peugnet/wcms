d30004f6dd381e454376039e5b306ab3767a8ed3
Julien Bidoret <julien@accentgrave.net>
2024-10-16 08:14:09 +0200

    Media progressive enhancement: drag and drop to fill regular file input

--- src/media.js
+++ src/media.js
@@ -4,3 +4,34 @@ window.addEventListener('load', () => {
     activateCheckall('id[]', 'checkall');
     activateCloseSubmenus();
 });
+
+
+// drag and drop trigger regular file input
+
+const fileInput = document.getElementById('file');
+const fileMenu = fileInput.closest('details');
+
+'drag dragstart dragend dragover dragenter dragleave drop'.split(' ').forEach(function(listener){
+    document.body.addEventListener(listener,function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    },false);
+});
+
+'dragover dragenter'.split(' ').forEach(function(listener){      
+    document.body.addEventListener(listener,function(e) {
+        fileMenu.setAttribute("open", true);
+    },false);
+});
+
+document.body.addEventListener('drop',function(e) {
+    const files = e.dataTransfer.files;
+    const info = document.createElement("p");
+    info.className = "dropped-files-info";
+    for(const f of files){ 
+        info.innerHTML += `<span>${f.name }</span> `; 
+    } 
+    fileInput.insertAdjacentElement("afterend", info);
+    fileInput.files = files;  
+}, false);
+
