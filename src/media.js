a0e055d0e6fdd97ce1dc8496345deb9cc418e600
Julien Bidoret <julien@accentgrave.net>
2024-10-16 11:14:08 +0200

    Media progressive enhancement: drag and drop to fill regular file input (fixed as prettier targets es5)

--- src/media.js
+++ src/media.js
@@ -11,27 +11,40 @@ window.addEventListener('load', () => {
 const fileInput = document.getElementById('file');
 const fileMenu = fileInput.closest('details');
 
-'drag dragstart dragend dragover dragenter dragleave drop'.split(' ').forEach(function(listener){
-    document.body.addEventListener(listener,function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    },false);
+'drag dragstart dragend dragover dragenter dragleave drop'
+    .split(' ')
+    .forEach(function(listener) {
+        document.body.addEventListener(
+            listener,
+            function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+            },
+            false
+        );
+    });
+
+'dragover dragenter'.split(' ').forEach(function(listener) {
+    document.body.addEventListener(
+        listener,
+        function(e) {
+            fileMenu.setAttribute('open', true);
+        },
+        false
+    );
 });
 
-'dragover dragenter'.split(' ').forEach(function(listener){      
-    document.body.addEventListener(listener,function(e) {
-        fileMenu.setAttribute("open", true);
-    },false);
-});
-
-document.body.addEventListener('drop',function(e) {
-    const files = e.dataTransfer.files;
-    const info = document.createElement("p");
-    info.className = "dropped-files-info";
-    for(const f of files){ 
-        info.innerHTML += `<span>${f.name }</span> `; 
-    } 
-    fileInput.insertAdjacentElement("afterend", info);
-    fileInput.files = files;  
-}, false);
-
+document.body.addEventListener(
+    'drop',
+    function(e) {
+        const files = e.dataTransfer.files;
+        const info = document.createElement('p');
+        info.className = 'dropped-files-info';
+        for (const f of files) {
+            info.innerHTML += `<span>${f.name}</span> `;
+        }
+        fileInput.insertAdjacentElement('afterend', info);
+        fileInput.files = files;
+    },
+    false
+);
