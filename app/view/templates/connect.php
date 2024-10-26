9540ed02ec0b7d4013c0725015a859e43a479126
Julien Bidoret <julien@accentgrave.net>
2024-10-26 13:34:13 +0200

    Connect view: removed .block (unnecessary), add class .button to back button (better match the input[submit] style)

--- app/view/templates/connect.php
+++ app/view/templates/connect.php
@@ -3,40 +3,37 @@
 
 <?php $this->start('page') ?>
 
-<div class="block connect">
+<div class="connect">
     <h2>Login</h2>
 
     <?php if($user->isvisitor()) : ?>
 
-    <form action="<?= $this->url('log') ?>" method="post">
-    <input type="hidden" name="route" value="<?= $route ?>">
-    <?php if(in_array($route, ['pageedit', 'pageread', 'pageadd'])) : ?>
-        <input type="hidden" name="id" value="<?= $id ?>">
-    <?php endif ?>
-    <form action="<?= $this->url('log') ?>" method="post" id="connect">
-    <input type="text" name="user" id="loginuser" autofocus placeholder="user" required>
-    <input type="password" name="pass" id="loginpass" placeholder="password" required>
-    <input type="hidden" name="rememberme" value="0">
-    <span>
-        <input type="checkbox" name="rememberme" id="rememberme" value="1">
-        <label for="rememberme">Remember me</label>
-    </span>
-    <input name="log" type="submit" value="login">
-    </form>
-
+        <form action="<?= $this->url('log') ?>" method="post">
+            <input type="hidden" name="route" value="<?= $route ?>">
+            <?php if(in_array($route, ['pageedit', 'pageread', 'pageadd'])) : ?>
+                <input type="hidden" name="id" value="<?= $id ?>">
+            <?php endif ?>
+            <form action="<?= $this->url('log') ?>" method="post" id="connect">
+            <input type="text" name="user" id="loginuser" autofocus placeholder="user" required>
+            <input type="password" name="pass" id="loginpass" placeholder="password" required>
+            <input type="hidden" name="rememberme" value="0">
+            <span>
+                <input type="checkbox" name="rememberme" id="rememberme" value="1">
+                <label for="rememberme">Remember me</label>
+            </span>
+            <input name="log" type="submit" value="login">
+        </form>
 
     <?php else : ?>    
 
-    <form action="<?= $this->url('log') ?>" method="post">
-    <input name="log" type="submit" value="logout">
-    </form>
-
-
+        <form action="<?= $this->url('log') ?>" method="post">
+            <input name="log" type="submit" value="logout">
+        </form>
 
     <?php endif ?>
 
     <?php if(in_array($route, ['pageedit', 'pageread', 'pageadd'])) : ?>
-        <p><a href="<?= $this->upage('pageread', $id) ?>">back to page read view</a></p>
+        <p><a class="button" href="<?= $this->upage('pageread', $id) ?>">back to page read view</a></p>
     <?php endif ?>
 </div>
 
