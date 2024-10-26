3e0c35ecd6b89187067d5e955d21faa5f4beff25
Julien Bidoret <julien@accentgrave.net>
2024-10-26 14:10:37 +0200

    Profile view: switch to grid layout, via main.grid > .grid-item Profile view: wrapping of each label+input in a .field Profile view: add .info class so informative paragraphs Profile view: removed unnecessary div.block

--- app/view/templates/profile.php
+++ app/view/templates/profile.php
@@ -1,121 +1,96 @@
 <?php
-
-use Wcms\Model;
-
-$this->layout('layout', ['title' => 'profile', 'stylesheets' => [$css . 'back.css', $css . 'profile.css']]) ?>
-
+    use Wcms\Model;
+    $this->layout('layout', ['title' => 'profile', 'stylesheets' => [$css . 'back.css', $css . 'profile.css']]) 
+?>
 
 <?php $this->start('page') ?>
-
 <?php $this->insert('backtopbar', ['user' => $user, 'tab' => 'profile', 'pagelist' => $pagelist]) ?>
 
+<main class="profile grid">
 
-<main class="profile">
-
-    <section id="profile">
-        <div class="block">
-
+    <div class="grid-item" id="profile">
             
-            <h1><i class="fa fa-user"></i> User profile</h1>
+        <h2><span><i class="fa fa-user"></i> User profile</span></h2>
+        <table>
+            <tbody>
+                <tr>
+                    <td>id</td>
+                    <td><?= $user->id() ?></td>
+                </tr>
+                <tr>
+                    <td>connection counter</td>
+                    <td><?= $user->connectcount() ?></td>
+                </tr>
+                <tr>
+                    <td>account expiration</td>
+                    <td><?= $user->expiredate('hrdi') ?></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+
+    <div class="grid-item" id="preferences">
+
+        <h2>Preferences</h2>
+
+        <form action="<?= $this->url('profileupdate') ?>" method="post" id="preferences">
+
+            <p class="info">Change some infos about you.</p>
+
+            <p class="field">
+                <label for="name">Display name (if none, user ID will be used)</label>
+                <input type="text" name="name" id="name" value="<?= $user->name() ?>" placeholder="<?= $user->id() ?> " maxlength="<?= Wcms\Item::LENGTH_SHORT_TEXT ?>">
+            </p>
             
-            <div class="scroll">
-                
-                <table>
-                    <thead>
-                        <tr>
-                            <th>stat</th>
-                            <th>value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>id</td>
-                            <td><?= $user->id() ?></td>
-                        </tr>
-                        <tr>
-                            <td>connection counter</td>
-                            <td><?= $user->connectcount() ?></td>
-                        </tr>
-                        <tr>
-                            <td>account expirations</td>
-                            <td><?= $user->expiredate('hrdi') ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+            <p class="field">
+                <label for="url">Associated url (can be a page ID)</label>
+                <input type="text" name="url" id="url" value="<?= $user->url() ?>" list="searchdatalist" placeholder="URL or page ID" maxlength="<?= Wcms\Item::LENGTH_SHORT_TEXT ?>" >
+            </p>
+            
+            <p class="info">When you tick the <em>remember-me</em> checkbox during login, you can choose how much time <strong>W</strong> will remember you.</p>
 
-                <h2>Preferences</h2>
+            <p class="field">
+                <label for="cookie">Cookie conservation time <i>(In days)</i></label>
+                <input type="number" name="cookie" value="<?= $user->cookie() ?>" id="cookie" min="0" max="<?= Model::MAX_COOKIE_CONSERVATION ?>" >
+            </p>
+            
+            <p class="field submit-field">
+                <input type="submit" value="update preferences">
+            </p>
+            
+        </form>
+    </div>
 
+    <div class="grid-item" id="preferences">
+        <h2>Password</h2>
 
-                <form action="<?= $this->url('profileupdate') ?>" method="post" id="preferences">
+        <form action="<?= $this->url('profilepassword') ?>" method="post" id="password">
 
-                    <p>Change some infos about you.</p>
+            <p class="info">Password have to be between <?= Wcms\Model::PASSWORD_MIN_LENGTH ?> and <?= Wcms\Model::PASSWORD_MAX_LENGTH ?> characters long.</p>
 
-                    <label for="name">Display name (if none, user ID will be used)</label>
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        value="<?= $user->name() ?>"
-                        placeholder="<?= $user->id() ?> "
-                        maxlength="<?= Wcms\Item::LENGTH_SHORT_TEXT ?>"
-                    >
-                    <br>
+            <p class="field">
+                <label for="currentpassword">Actual password</label>
+                <input type="password" name="currentpassword" id="currentpassword" required>
+            </p>
 
+            <p class="field">
+                <label for="password1">New password</label>
+                <input type="password" name="password1" id="password1" minlength="<?= Wcms\Model::PASSWORD_MIN_LENGTH ?>" required>
+            </p>
 
-                    <label for="url">associated url (can be a page ID)</label>
-                    <input
-                        type="text"
-                        name="url"
-                        id="url"
-                        value="<?= $user->url() ?>"
-                        list="searchdatalist"
-                        placeholder="URL or page ID"
-                        maxlength="<?= Wcms\Item::LENGTH_SHORT_TEXT ?>"
-                    >
-                    <br>
+            <p class="field">
+                <label for="password2">Confirm new password</label>
+                <input type="password" name="password2" id="password2" minlength="<?= Wcms\Model::PASSWORD_MIN_LENGTH ?>" required>
+            </p>
 
-                    <p>When you tick the <em>remember-me</em> checkbox during login, you can choose how much time <strong>W</strong> will remember you.</p>
-                    <input
-                        type="number"
-                        name="cookie"
-                        value="<?= $user->cookie() ?>"
-                        id="cookie"
-                        min="0"
-                        max="<?= Model::MAX_COOKIE_CONSERVATION ?>"
-                    >
-                    <label for="cookie">Cookie conservation time <i>(In days)</i></label>
-                    <input type="submit" value="update preferences">
-                    
-                </form>
+            <p class="field">
+                <input type="submit" value="update password">
+            </p>
 
-                <h2>Password</h2>
+        </form>
 
-
-                <form action="<?= $this->url('profilepassword') ?>" method="post" id="password">
-
-                    <p>Password have to be between <?= Wcms\Model::PASSWORD_MIN_LENGTH ?> and <?= Wcms\Model::PASSWORD_MAX_LENGTH ?> characters long.</p>
-
-                    <label for="currentpassword">Actual password</label>
-                    <input type="password" name="currentpassword" id="currentpassword" required>
-
-                    <label for="password1">New password</label>
-                    <input type="password" name="password1" id="password1" minlength="<?= Wcms\Model::PASSWORD_MIN_LENGTH ?>" required>
-                    <label for="password2">Confirm new password</label>
-                    <input type="password" name="password2" id="password2" minlength="<?= Wcms\Model::PASSWORD_MIN_LENGTH ?>" required>
-                    <input type="submit" value="update password">
-
-
-
-                </form>
-
-            </div>
-
-
-        </div>
-
-    </section>
-
-    
+    </div>   
 
 </main>
 
