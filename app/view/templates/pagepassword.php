cbc9af39b6d8e8e6bd3f463d24c64c033418f1c9
Julien Bidoret <julien@accentgrave.net>
2024-10-26 14:56:00 +0200

    Password protected page: cleaner HTML Password protected page: added a submit button

--- app/view/templates/pagepassword.php
+++ app/view/templates/pagepassword.php
@@ -2,39 +2,33 @@
 
 $this->layout('readerlayout') ?>
 
-<?php
-$this->start('head');
-?>
+<?php $this->start('head'); ?>
 
-<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>🔑</title>
+
     <?= Wcms\Config::alertcss() ? '<link href="' . Wcms\Model::dirtopath(Wcms\Model::ASSETS_CSS_DIR) . 'global.css" rel="stylesheet" />' : '' ?>
     <meta name="viewport" content="width=device-width">
-</head>
 
-
-<?php
-$this->stop();
-?>
-
-
+<?php $this->stop(); ?>
 
 <?php $this->start('page') ?>
 
-<body class="alert">
+    <body class="alert">
 
-<main class="alert">
+        <main class="alert">
 
+            <h1>This page is password protected</h1>
 
-<h1>This page is password protected</h1>
+            <form action="<?= $this->url('pagereadpost', ['page' => $pageid]) ?>" method="post">
+                <label for="pagepassword">Page password</label>
+                <input type="password" name="pagepassword" id="pagepassword" autofocus required>
+                <input type="submit" value="OK">
+            </form>
 
-<form action="<?= $this->url('pagereadpost', ['page' => $pageid]) ?>" method="post">
-<label for="pagepassword">Page password</label>
-<input type="password" name="pagepassword" id="pagepassword" autofocus required>
-</form>
+        </main>
 
-</main>
-
-
-</body>
+    </body>
 
 <?php $this->stop() ?>
