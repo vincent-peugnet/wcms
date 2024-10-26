201bf5a3f08995a63cb86f7320525f8662fe950b
Julien Bidoret <julien@accentgrave.net>
2024-10-26 13:49:09 +0200

    Home view: Bookmarks: Replaced table layout by flexcol, and wrapped each bookmark in a <p class="bookmark"> to achieve better layout of each bookmark.

--- app/view/templates/homebookmark.php
+++ app/view/templates/homebookmark.php
@@ -5,60 +5,42 @@
     <div class="toggle-panel" id="optionspanel">
         <h2>Bookmarks</h2>
         <div class="toggle-panel-content">
-            <h3>public</h3>
-
-            <table>
-                <tbody>
-                    <?php foreach ($publicbookmarks as $bookmark) : ?>
-                        <tr>
-                            <td>
-                                <a
-                                    href="<?= $this->url("home", [], $bookmark->query()) ?>&display=<?= $display ?>"
-                                    data-current="<?= isset($queryaddress) && $bookmark->query() === $queryaddress ? '1' : '0' ?>"
-                                    class="bookmark"
-                                    title="<?= $bookmark->description() ?>"
-                                >
-                                    <span class="icon">
-                                        <?= $bookmark->icon() ?>
-                                    </span>
-                                    <span class="name">
-                                        <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
-                                    </span>
-                                </a>
-                            </td>
-                            <td class="rss">
-                                <?php if($bookmark->ispublished()) : ?>
-                                    <a href="<?= Wcms\Servicerss::atomfile($bookmark->id()) ?>" target="_blank" title="show Atom XML file">
-                                        <i class="fa fa-rss"></i>
-                                    </a>
-                                <?php endif ?>
-                            </td>
-                        </tr>
-                    <?php endforeach ?>
-                </tbody>
-            </table>
-
-                <h3>personal</h3>
-            
-            <table>
-                <?php foreach ($personalbookmarks as $bookmark) : ?>
-                    <tr>
-                        <td>
-                            <a
-                                href="<?= $this->url("home", [], $bookmark->query()) ?>&display=<?= $display ?>"
-                                data-current="<?= isset($queryaddress) && $bookmark->query() === $queryaddress ? '1' : '0' ?>"
-                                class="bookmark"
-                                title="<?= $bookmark->description() ?>"
-                            >
-                                <?= $bookmark->icon() ?>
-                                <span>
-                                    <?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?>
-                                </span>
+            <div class="flexcol">
+                <h3>Public</h3>
+                <?php foreach ($publicbookmarks as $bookmark) : ?>
+                    <p class="bookmark">
+                        <a
+                            href="<?= $this->url("home", [], $bookmark->query()) ?>&display=<?= $display ?>"
+                            data-current="<?= isset($queryaddress) && $bookmark->query() === $queryaddress ? '1' : '0' ?>"
+                            class="bookmark"
+                            title="<?= $bookmark->description() ?>"
+                        >
+                            <span class="icon"><?= $bookmark->icon() ?></span>
+                            <span class="name"><?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?></span>
+                        </a>                            
+                        <?php if($bookmark->ispublished()) : ?>
+                            <a href="<?= Wcms\Servicerss::atomfile($bookmark->id()) ?>" target="_blank" title="show Atom XML file">
+                                <i class="fa fa-rss"></i>
                             </a>
-                        </td>
-                    </tr>
+                    </p>
+                    <?php endif ?>
                 <?php endforeach ?>
-            </table>
+        
+                <h3>Personal</h3>
+                <?php foreach ($personalbookmarks as $bookmark) : ?>
+                    <p class="bookmark">
+                        <a
+                            href="<?= $this->url("home", [], $bookmark->query()) ?>&display=<?= $display ?>"
+                            data-current="<?= isset($queryaddress) && $bookmark->query() === $queryaddress ? '1' : '0' ?>"
+                            class="bookmark"
+                            title="<?= $bookmark->description() ?>"
+                        >
+                            <span class="icon"><?= $bookmark->icon() ?></span>
+                            <span class="name"><?= empty($bookmark->name()) ? $bookmark->id() : $bookmark->name() ?></span>
+                        </a>
+                    </p>
+                <?php endforeach ?>
+            </div>
         </div>
     </div>
 </aside>
