6b310222b4f79be03ff6775a41d1186bac3766c6
Julien Bidoret <julien@accentgrave.net>
2024-10-26 14:58:27 +0200

    Delete user view: cleaner structure, matching delete page view Delete user view: Cancel / back button relying on $_SERVER['HTTP_REFERER'] or on javascript if not available.

--- app/view/templates/userconfirmdelete.php
+++ app/view/templates/userconfirmdelete.php
@@ -1,36 +1,36 @@
+<?php $this->layout('layout', ['title' => 'delete', 'description' => 'delete', 'stylesheets' => [$css . 'home.css', $css . 'back.css']]) ?>
+
+<?php $this->start('page') ?>
 
 <?php if($candelete) : ?>
 
+    <h1>Delete user “<?= $userdelete->id() ?>”&#8239;</h1>
 
-    <h1>Delete User</h1>
+    <div class="confirm-delete">
 
-    <h2>Id : <?= $userdelete->id() ?></h2>
-    <h2>Level : <?= $userdelete->level() ?></h2>
+        <p>Id : <?= $userdelete->id() ?></p>
+        <p>Level : <?= $userdelete->level() ?></p>
 
+        <form action="<?= $this->url('useredit') ?>" method="post">
+            <input type="hidden" name="id" value="<?= $userdelete->id() ?>">
+            <input type="submit" name="action" value="Confirm delete">
+            
+            <?php $previous = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "javascript:history.go(-1)"; ?>
+            <a class="button" href="<?= $previous ?>">Cancel</a>
+        </form>
 
-
-    <form action="<?= $this->url('useredit') ?>" method="post">
-
-    <input type="hidden" name="id" value="<?= $userdelete->id() ?>">
-
-    <input type="submit" name="action" value="confirmdelete">
-
-
-    </form>
-
-
-
-
+    </div>
 
 <?php else : ?>
 
     <h1>You can't delete this user</h1>
 
-    <h2>You can't delete yourself</h2>
-
-    <p>To delete this user, create at least another admin user, log in as this other admin user, the try to delete this user.</p>
-
-    <a href="<?= $this->url('user') ?>">Go back to users</a>
-
+    <div class="confirm-delete">
+        <p>You can't delete yourself!</p>
+        <p>To delete this user, create at least another admin user, log in as this other admin user, then try to delete this user.</p>
+        <p><a href="<?= $this->url('user') ?>" class="button">Go back to users</a></p>
+    </div>
 
 <?php endif ?>
+
+<?php $this->stop() ?>
