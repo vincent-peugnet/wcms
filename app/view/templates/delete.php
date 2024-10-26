25dc89a43b8bf2de9c07f8d752f343948c012cd3
Julien Bidoret <julien@accentgrave.net>
2024-10-26 13:41:04 +0200

    Delete page view: cleaner layout, with <p>s instead of <ul> Delete page view: Cancel / back button relying on $_SERVER['HTTP_REFERER'] or on javascript if not available. Delete page view: public page can be accessed through a dedicated link, showing its  URL

--- app/view/templates/delete.php
+++ app/view/templates/delete.php
@@ -1,42 +1,33 @@
 <?php $this->layout('layout', ['title' => 'delete', 'description' => 'delete', 'stylesheets' => [$css . 'home.css', $css . 'back.css']]) ?>
 
-
 <?php $this->start('page') ?>
 
+<h1>Delete page “<?= $page->id() ?>”&#8239;</h1>
 
+<div class="confirm-delete">
 
-<div class="block">
+    <p>URL : <a href="<?= $this->upage('pageread', $page->id()) ?>" ><?= $this->upage('pageread', $page->id()) ?></a></p>
+    <p>Id : <?= $page->id() ?></p>
+    <p>Title : <?= $page->title() ?></p>
+    <p>Number of edits : <?= $page->editcount() ?></p>
+    <p>Number of displays : <?= $page->displaycount() ?></p>
+    <p>
+        Page linking to this one : <?= $pageslinkingtocount ?>
+        <?php if ($pageslinkingtocount > 0) : ?>
+            <a href="<?= $this->url('home', [], '?linkto=' . $page->id() . '&submit=filter') ?>" title="search for pages linking to this one in home view">
+                <i class="fa fa-search"></i>
+            </a>
+        <?php endif ?>
+    </p>   
 
-    <h1>Delete</h1>
-
-    <ul>
-        <li>Id : <?= $page->id() ?></li>
-        <li>Title : <?= $page->title() ?></li>
-        <li>Number of edits : <?= $page->editcount() ?></li>
-        <li>Number of displays : <?= $page->displaycount() ?></li>
-        <li>
-            Page linking to this one : <?= $pageslinkingtocount ?>
-            <?php if ($pageslinkingtocount > 0) : ?>
-                <a href="<?= $this->url('home', [], '?linkto=' . $page->id() . '&submit=filter') ?>" title="search for pages linking to this one in home view">
-                    <i class="fa fa-search"></i>
-                </a>
-            <?php endif ?>
-        </li>
-    </ul>
-    
-
-
-
-    
-    
     <form action="<?= $this->upage('pagedelete', $page->id()) ?>" method="post">
         <input type="hidden" name="deleteconfirm" value="true">
-        <input type="submit" value="confirm delete">
+        <input type="submit" value="Confirm delete">
+        
+        <?php $previous = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "javascript:history.go(-1)"; ?>
+        <a class="button" href="<?= $previous ?>">Cancel</a>
     </form>
 
-    
 </div>
-<a href="<?= $this->upage('pageread', $page->id()) ?>">back to page</a>
-
 
 <?php $this->stop() ?>
