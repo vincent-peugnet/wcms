vincent-peugnet/wcms

commit 3b3413a3f3a513b04321088051ff410d7e4510d3

fix page version identificator and upgrade session edit rescue
- detect content element in case of missing version metadata
- add version as hidden input in edit view
- automatically save page to database if it was saved from session

app/class/Modelpage.php
@@ -901,6 +901,8 @@ class Modelpage extends Modeldb
                     return new Pagev2($datas);
             }
             throw new RangeException('Version is specified but out of range');
+        } elseif (isset($datas['content'])) {
+            return new Pagev2($datas);
         } else {
             return new Pagev1($datas);
         }

app/class/Controllerpage.php
@@ -53,10 +53,11 @@ class Controllerpage extends Controller
     protected function importpage(): bool
     {
         try {
-            if (isset($_SESSION['pageupdate']['id']) && $_SESSION['pageupdate']['id'] == $this->page->id()) {
+            if (isset($_SESSION['pageupdate']['id']) && $_SESSION['pageupdate']['id'] === $this->page->id()) {
                 $this->page = $this->pagemanager->parsepage($_SESSION['pageupdate']);
+                $this->pagemanager->update($this->page);
                 unset($_SESSION['pageupdate']);
-                return true;
+                $this->routedirect('pageedit', ['page' => $this->page->id()]);
             } else {
                 $this->page = $this->pagemanager->get($this->page);
                 return true;

app/view/templates/editleftbar.php
@@ -6,8 +6,6 @@
         <h1>Meta</h1>
         <div class="toggle-panel-content">
 
-            <input type="hidden" name="datemodif" value="<?= $page->datemodif('string') ?>" form="update">
-
             <details id="editinfo" open>
                 <summary>Informations</summary>
                 <fieldset >                        

app/view/templates/edittopbar.php
@@ -11,6 +11,9 @@
                     <span id="editstatus"></span>
                 </span>
             </button>
+
+            <input type="hidden" name="datemodif" value="<?= $page->datemodif('string') ?>">
+            <input type="hidden" name="version" value="<?= $page->version() ?>">
         </form>
 
         <a href="<?= $this->upage('pageread', $page->id()) ?>" target="<?= $target ?>" id="display">

commit b966af6ca735bd7a22a88c80ce2a4fb65d62a4c6

fix parsepage bug
caused recently in 3b3413a

app/class/Modelpage.php
@@ -901,7 +901,7 @@ class Modelpage extends Modeldb
                     return new Pagev2($datas);
             }
             throw new RangeException('Version is specified but out of range');
-        } elseif (isset($datas['content'])) {
+        } elseif (isset($metadatas['content'])) {
             return new Pagev2($datas);
         } else {
             return new Pagev1($datas);