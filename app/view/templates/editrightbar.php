84f7a410d168cd88daf41895464a47257c1907a3
vincent-peugnet <vincent-peugnet@riseup.net>
2025-09-10 17:25:57 +0200

    edit right bar use summary details
    which allow to start with all of them closed on mobile view

--- app/view/templates/editrightbar.php
+++ app/view/templates/editrightbar.php
@@ -4,73 +4,90 @@
 
     <div class="toggle-panel" id="rightbarpanel">
 
-        <h2>Infos</h2>
+        <h1>Infos</h1>
     
-        <div class="toggle-panel-content flexcol">
-            <h3>Stats</h3>
+        <div class="toggle-panel-content">
+            <details id="stats" <?= $workspace->collapsemenu() ? '' : 'open' ?>>
+                <summary>Stats</summary>
 
-            <table>
-                <tbody>
-                    <tr>
-                        <td>edition:</td>
-                        <td><?= $page->editcount() ?></td>
-                    </tr>
-                    <tr>
-                        <td>display:</td>
-                        <td><?= $page->displaycount() ?></td>
-                    </tr>
-                    <tr>
-                        <td>visit:</td>
-                        <td><?= $page->visitcount() ?></td>
-                    </tr>
-                </tbody>
-            </table>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>edition</td>
+                            <td><?= $page->editcount() ?></td>
+                        </tr>
+                        <tr>
+                            <td>display</td>
+                            <td><?= $page->displaycount() ?></td>
+                        </tr>
+                        <tr>
+                            <td>visit</td>
+                            <td><?= $page->visitcount() ?></td>
+                        </tr>
+                    </tbody>
+                </table>
 
-            <h3>internal links: <?= count($page->linkto()) ?></h3>
+                <p class="field">
+                    <a href="<?= $this->url('home', [], $homebacklink) ?>" class="button">
+                        <i class="fa fa-list-ul"></i>
+                        list pages that link here
+                    </a>
+                </p>
+            </details>
 
-            <ul class="internallinks">
-                <?php foreach ($page->linkto() as $link) : ?>
-                    <li>
-                        <a href="<?= $this->upage('pageread', $link) ?>" class="read" target="_blank" <?= strlen($link) > 30 ? "title=\"$link\"" : '' ?>>
-                            <?= $link ?>
-                        </a>
-                        <a href="<?= $this->upage('pageedit', $link) ?>" class="button">
-                            <i class="fa fa-pencil"></i>
-                        </a>
-                    </li>
-                <?php endforeach ?>
-            </ul>
 
-            <h3>back links</h3>
+            <details id="internal-links" <?= $workspace->collapsemenu() ? '' : 'open' ?>>
+                <summary>internal links (<?= count($page->linkto()) ?>)</summary>
 
-            <a href="<?= $this->url('home', [], $homebacklink) ?>" class="button">
-                <i class="fa fa-list-ul"></i>
-                list pages that link here
-            </a>
+                <table>
+                    <tbody>
+                        <?php foreach ($page->linkto() as $link) : ?>
+                            <tr>
+                                <td class="id" <?= strlen($link) > 30 ? "title=\"$link\"" : '' ?>>
+                                    <?= $link ?>
+                                </td>
+                                <td class="read">
+                                    <a href="<?= $this->upage('pageread', $link) ?>" class="button" target="_blank">
+                                        <i class="fa fa-eye"></i>
+                                    </a>
+                                </td>
+                                <td class="edit">
+                                    <a href="<?= $this->upage('pageedit', $link) ?>" class="button">
+                                        <i class="fa fa-pencil"></i>
+                                    </a>
+                                </td>
+                        </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
 
-            <h3>external links: <?= count($page->externallinks()) ?></h3>
+            </details>
 
-            <ul class="externallinks">
-                <?php foreach ($page->externallinks() as $url => $status) : ?>
-                    <?php if(Wcms\Config::urlchecker() && key_exists($url, $urls)) : ?>
-                        <li title="<?= $url ?>&#013;<?= $urls[$url]['response'] < 100 ? '❌' : '💡 ' . $urls[$url]['response']  ?> <?= $urls[$url]['message'] ?>&#013;⏱️ checked <?= hrdi($urls[$url]['timestamp']->diff($now)) ?> ago">
-                    <?php else : ?>
-                        <li title="<?= $url ?>">
-                    <?php endif ?>
-                        <a href="<?= $url ?>" target="_blank">
-                            <?= ltrim(substr($url, 6), "\/") ?>
-                        </a>
-                        <span>
-                            <?= is_bool($status) ? ($status ? '✅' : '💀') : '' ?>
-                        </span>
-                    </li>
-                <?php endforeach ?>
-            </ul>
+            <details id="external-links" <?= $workspace->collapsemenu() ? '' : 'open' ?>>
+                <summary>external links (<?= count($page->externallinks()) ?>)</summary>
 
-            <h3>Help</h3>
-            <div id="help">
+                <ul class="externallinks">
+                    <?php foreach ($page->externallinks() as $url => $status) : ?>
+                        <?php if(Wcms\Config::urlchecker() && key_exists($url, $urls)) : ?>
+                            <li title="<?= $url ?>&#013;<?= $urls[$url]['response'] < 100 ? '❌' : '💡 ' . $urls[$url]['response']  ?> <?= $urls[$url]['message'] ?>&#013;⏱️ checked <?= hrdi($urls[$url]['timestamp']->diff($now)) ?> ago">
+                        <?php else : ?>
+                            <li title="<?= $url ?>">
+                        <?php endif ?>
+                            <a href="<?= $url ?>" target="_blank">
+                                <?= ltrim(substr($url, 6), "\/") ?>
+                            </a>
+                            <span>
+                                <?= is_bool($status) ? ($status ? '✅' : '💀') : '' ?>
+                            </span>
+                        </li>
+                    <?php endforeach ?>
+                </ul>
+            </details>
+
+            <details id="help" <?= $workspace->collapsemenu() ? '' : 'open' ?>>
+                <summary>Help</summary>
                 <?php $this->insert('edithelp') ?>
-            </div>           
+            </details>
         </div>
 
     </div>
