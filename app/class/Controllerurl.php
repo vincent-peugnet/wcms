77b98ca3914d285bd72a1ef01e0488af7c10b6a3
vincent-peugnet <vincent-peugnet@riseup.net>
2026-01-18 20:24:24 +0100

    move urlcache routes under controllerurl

--- app/class/Controllerurl.php
+++ app/class/Controllerurl.php
@@ -4,6 +4,7 @@ namespace Wcms;
 
 use AltoRouter;
 use RuntimeException;
+use Wcms\Exception\Filesystemexception\Notfoundexception;
 
 class Controllerurl extends Controller
 {
@@ -60,6 +61,48 @@ class Controllerurl extends Controller
             $this->sendflashmessage('an error occured: ' . $e->getMessage(), self::FLASH_ERROR);
         }
 
+        $this->routedirect('url');
+    }
+
+    public function flushurlcache(): never
+    {
+        if (!$this->user->isadmin()) {
+            http_response_code(304);
+            $this->showtemplate('forbidden');
+        }
+        try {
+            Fs::deletefile(Model::URLS_FILE);
+            $this->sendflashmessage('URL cache successfully deleted', self::FLASH_SUCCESS);
+            $user = $this->user->id();
+            Logger::info("URL cache successfully deleted by user '$user'");
+        } catch (Notfoundexception $e) {
+            $this->sendflashmessage('URL cache is already deleted ' . $e->getMessage(), self::FLASH_WARNING);
+        } catch (RuntimeException $e) {
+            $msg = 'Error while trying to flush page render cache:' . $e->getMessage();
+            $this->sendflashmessage($msg, self::FLASH_ERROR);
+            Logger::error($msg);
+        }
+        $this->routedirect('url');
+    }
+
+    /**
+     * Remove unused URLs from cache
+     */
+    public function cleanurlcache(): never
+    {
+        if (!$this->user->issupereditor()) {
+            http_response_code(304);
+            $this->showtemplate('forbidden');
+        }
+        try {
+            $urlchecker = new Serviceurlchecker(0);
+            $removed = $urlchecker->cleancache($this->pagemanager);
+            $urlchecker->savecache();
+            $this->sendflashmessage("$removed unused url(s) where removed from cache.", self::FLASH_SUCCESS);
+        } catch (RuntimeException $e) {
+            $this->sendflashmessage($e, self::FLASH_ERROR);
+            Logger::errorex($e);
+        }
         $this->routedirect('url');
     }
 }
