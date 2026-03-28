vincent-peugnet/wcms

commit 0a02760acbb1119087bcc73cd3e4bd76d8052a07

prevent page comments metada edition during page updates
cases:
- no-JS update
- API update
- API put

app/class/Controllerapipage.php
@@ -108,6 +108,10 @@ class Controllerapipage extends Controllerapi
             $this->shortresponse(409, "Conflict: A more recent version of the page is stored in the database");
         }
 
+        // comment count and comment date should not be modified by user update
+        $this->page->setcommentcount($oldpage->commentcount());
+        $this->page->datecomment($oldpage->datecomment());
+
         try {
             $this->page->updateedited();
             $this->page->addauthor($this->user->id()); // prevent editor from removing itself from authors
@@ -157,11 +161,16 @@ class Controllerapipage extends Controllerapi
             $this->shortresponse(401, 'Page already exist but user cannot update it');
         }
         try {
-            $this->page = $this->pagemanager->newpage(array_merge($this->recievejson(), ['id' => $page]));
-            if (!$exist) { // If it's a page creation, add the user as an author
-                $this->page->addauthor($this->user->id());
+            $putpage = $this->pagemanager->newpage(array_merge($this->recievejson(), ['id' => $page]));
+            if (!$exist) { // If it's a page creation, add the user as an author and reset comments metadata
+                $putpage->addauthor($this->user->id());
+                $putpage->setcommentcount(0);
+                $putpage->setdatecomment(null);
+            } else { // otherwise we prevent comment metada edition
+                $putpage->setcommentcount($this->page->commentcount());
+                $putpage->setdatecomment($this->page->datecomment());
             }
-            $this->pagemanager->add($this->page);
+            $this->pagemanager->add($putpage);
             http_response_code($exist ? 200 : 201);
             if ($exist) {
                 $user = $this->user->id();

app/class/Controllerpage.php
@@ -606,6 +606,10 @@ class Controllerpage extends Controller
             $this->routedirect('pageedit', ['page' => $this->page->id()]);
         }
 
+        // comment count and comment date should not be modified by user update
+        $this->page->setcommentcount($oldpage->commentcount());
+        $this->page->datecomment($oldpage->datecomment());
+
         try {
             $this->page->updateedited();
             $this->page->addauthor($this->user->id()); // prevent editor from removing itself from authors

app/class/Page.php
@@ -480,6 +480,9 @@ abstract class Page extends Item
         return $this->noindex;
     }
 
+    /**
+     * @return int<0, max> $commentcount
+     */
     public function commentcount(): int
     {
         return $this->commentcount;
@@ -879,6 +882,9 @@ abstract class Page extends Item
         $this->externallinks = $externallinks;
     }
 
+    /**
+     * @param int<0, max> $commentcount
+     */
     public function setcommentcount(int $commentcount): void
     {
         if ($commentcount >= 0) {

commit 5c6503a944bf74abb323e58e0b2da90f701b9b10

fix typo that cause bug during page update
introduced in 0a02760acbb1119087bcc73cd3e4bd76d8052a07

app/class/Controllerapipage.php
@@ -110,7 +110,7 @@ class Controllerapipage extends Controllerapi
 
         // comment count and comment date should not be modified by user update
         $this->page->setcommentcount($oldpage->commentcount());
-        $this->page->datecomment($oldpage->datecomment());
+        $this->page->setdatecomment($oldpage->datecomment());
 
         try {
             $this->page->updateedited();

app/class/Controllerpage.php
@@ -608,7 +608,7 @@ class Controllerpage extends Controller
 
         // comment count and comment date should not be modified by user update
         $this->page->setcommentcount($oldpage->commentcount());
-        $this->page->datecomment($oldpage->datecomment());
+        $this->page->setdatecomment($oldpage->datecomment());
 
         try {
             $this->page->updateedited();