vincent-peugnet/wcms

commit f20f058c06185d9c9a2a348c0c0c52c53b3050f6

comment controller: user who cannot read the page cannot comment

app/class/Controller.php
@@ -304,6 +304,16 @@ abstract class Controller
     }
 
     /**
+     * Tell if the current user can read the given Page
+     *
+     * @param Page $page
+     */
+    protected function canread(Page $page): bool
+    {
+        return $this->user->level() < $page->secure();
+    }
+
+    /**
      * Tell if the current user can edit the given Page
      *
      * User need to be SUPEREDITOR, otherwise, it need to be author of a page.

app/class/Controllerpage.php
@@ -158,7 +158,7 @@ class Controllerpage extends Controller
             }
         }
 
-        if ($this->user->level() < $this->page->secure()) {
+        if ($this->canread($this->page)) {
             http_response_code(403);
             switch ($this->page->secure()) {
                 case Page::NOT_PUBLISHED:

app/class/Controllercomment.php
@@ -33,6 +33,12 @@ class Controllercomment extends Controller
             $this->showtemplate('alertcomment', ['message' => $e->getMessage()]);
         }
 
+        // users who cannot read the page cannot post comments
+        if (!$this->canread($page)) {
+            http_response_code(401);
+            $this->showtemplate('alertcomment', ['message' => 'unauthorized']);
+        }
+
 
         if (!isset($_POST[Modelcomment::CONFIG_POST_NAME])) {
             $msg = sprintf("comment on page '%s': missing config token", $page->id());
@@ -62,7 +68,7 @@ class Controllercomment extends Controller
 
         // check if visitors are allowed to comment
         if (!$conf->allowvisitor() && $this->user->isvisitor()) {
-            http_response_code(403);
+            http_response_code(401);
             $this->showtemplate('alertcomment', ['message' => 'comment not allowed from visitors']);
         }
 

commit a20839ab825fe00ec8308dfa78bf5f5360f2e823

fix and refactor controller->canread() method
fix bug introduced in f20f058c06185d9c9a2a348c0c0c52c53b3050f6 (previous commit)

add user level matrix in manual

new rule: invite users cannot read not_published page if not listed as authors

app/class/Controller.php
@@ -306,11 +306,28 @@ abstract class Controller
     /**
      * Tell if the current user can read the given Page
      *
+     * - PUBLIC pages can be read by anyone
+     * - EDITORS can read all page
+     * - PRIVATE pages need READERS or above
+     * - NOT_PUBLISHED pages need INVITE users listed as author of page
+     *
      * @param Page $page
      */
     protected function canread(Page $page): bool
     {
-        return $this->user->level() < $page->secure();
+        if ($page->ispublic()) {
+            return true;
+        }
+        if ($this->user->iseditor()) {
+            return true;
+        }
+        if ($page->isprivate() && $this->user->isreader()) {
+            return true;
+        }
+        if ($page->isnotpublished() && $this->user->isinvite() && in_array($this->user->id(), $page->authors())) {
+            return true;
+        }
+        return false;
     }
 
     /**

app/class/Controllerpage.php
@@ -158,7 +158,7 @@ class Controllerpage extends Controller
             }
         }
 
-        if ($this->canread($this->page)) {
+        if (!$this->canread($this->page)) {
             http_response_code(403);
             switch ($this->page->secure()) {
                 case Page::NOT_PUBLISHED:

app/class/Modeluser.php
@@ -9,13 +9,6 @@ use Wcms\Exception\Database\Notfoundexception;
 
 class Modeluser extends Modeldb
 {
-    public const ADMIN = 10;
-    public const SUPEREDITOR = 4;
-    public const EDITOR = 3;
-    public const INVITE = 2;
-    public const READ = 1;
-    public const FREE = 0;
-
     public const USER_REPO_NAME = 'user';
 
     public function __construct()
@@ -27,7 +20,7 @@ class Modeluser extends Modeldb
 
     public function logout(): User
     {
-        $user = new User(['level' => self::FREE]);
+        $user = new User(['level' => User::VISITOR]);
         return $user;
     }
 

app/class/User.php
@@ -37,12 +37,19 @@ class User extends Item
 
     protected string $theme = '';
 
+    public const ADMIN       = 10;
+    public const SUPEREDITOR = 4;
+    public const EDITOR      = 3;
+    public const INVITE      = 2;
+    public const READER      = 1;
+    public const VISITOR     = 0;
+
     public const LEVELS = [
-        1  => 'reader',
-        2  => 'invite editor',
-        3  => 'editor',
-        4  => 'super editor',
-        10 => 'admin',
+        self::READER      => 'reader',
+        self::INVITE      => 'invite editor',
+        self::EDITOR      => 'editor',
+        self::SUPEREDITOR => 'super editor',
+        self::ADMIN       => 'admin',
     ];
 
     public const HOME_COLUMNS = [
@@ -396,7 +403,15 @@ class User extends Item
      */
     public function isvisitor(): bool
     {
-        return $this->level === Modeluser::FREE;
+        return $this->level === self::VISITOR;
+    }
+
+    /**
+     * User is at least reader
+     */
+    public function isreader(): bool
+    {
+        return $this->level >= self::READER;
     }
 
     /**
@@ -404,7 +419,7 @@ class User extends Item
      */
     public function isinvite(): bool
     {
-        return $this->level >= Modeluser::INVITE;
+        return $this->level >= self::INVITE;
     }
 
     /**
@@ -412,7 +427,7 @@ class User extends Item
      */
     public function iseditor(): bool
     {
-        return $this->level >= Modeluser::EDITOR;
+        return $this->level >= self::EDITOR;
     }
 
     /**
@@ -420,7 +435,7 @@ class User extends Item
      */
     public function issupereditor(): bool
     {
-        return $this->level >= Modeluser::SUPEREDITOR;
+        return $this->level >= self::SUPEREDITOR;
     }
 
     /**
@@ -428,7 +443,7 @@ class User extends Item
      */
     public function isadmin(): bool
     {
-        return $this->level === Modeluser::ADMIN;
+        return $this->level === self::ADMIN;
     }
 
 